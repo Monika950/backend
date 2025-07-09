a7a7eab36dbbd4f8bcfefa21e80e59fdb3faa80a
Monika Georgieva <monikageorgieva950@gmail.com>
2025-07-09 16:26:28 +0300

    jwt token

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,15 +3,17 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     UserModule,
     JwtModule.register({
       global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
     }),
   ],
   controllers: [AuthController],
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -42,14 +42,18 @@ export class AuthService {
     };
   }
 
-  // async signIn(username: string, pass: string): Promise<any> {
-  //   const user = await this.userService.findOne(username);
-  //   if (user?.password !== pass) {
-  //     throw new UnauthorizedException();
-  //   }
-  //   const payload = { sub: user.id, username: user.username };
-  //   return {
-  //     access_token: await this.jwtService.signAsync(payload),
-  //   };
-  // }
+  async signIn(signInDto: SignInDto) {
+    const user = await this.authenticate(signInDto);
+    const payload = {
+      sub: user.id,
+      username: user.username,
+      email: user.email,
+    };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+      //   id: user.id,
+      //   username: user.username,
+      //   email: user.email,
+    };
+  }
 }
