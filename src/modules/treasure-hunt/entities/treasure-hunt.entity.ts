6df0375638f96636f65bc445520ca3f41beef6ba
Monika Georgieva <monikageorgieva950@gmail.com>
2025-07-14 12:49:09 +0300

    Update treasure-hunt.entity.ts

--- src/modules/treasure-hunt/entities/treasure-hunt.entity.ts
+++ src/modules/treasure-hunt/entities/treasure-hunt.entity.ts
@@ -1,7 +1,40 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class TreasureHunt {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column('uuid', { array: true })
+  owners: string[];
+
+  @Column('uuid', { array: true }) 
+  users: string[];
+
+  @Column('uuid', { array: true })
+  locations: string[];
+
+  @Column({ unique: true })
+  code: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  start: Date;
+
+  @Column()
+  end: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
