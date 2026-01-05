006a8b0cd04b8a894c73037fa667ecad16857aa0
Monika Georgieva <monikageorgieva950@gmail.com>
2026-01-05 10:46:26 +0200

    user answer

--- src/modules/user-answer/dto/create-user-answer.dto.ts
+++ src/modules/user-answer/dto/create-user-answer.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserAnswerDto {}
+import { IsUUID, IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserAnswerDto {
+  @IsUUID()
+  @ApiProperty({
+    description: 'Location identifier being answered',
+    example: 'a3f1c9b2-8d74-4f5e-9c1a-2b1e4d5f6a7b',
+  })
+  locationId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'User-provided answer text',
+    example: 'Hristo Botev',
+  })
+  answer: string;
+}
--- src/modules/user-answer/user-answer.module.ts
+++ src/modules/user-answer/user-answer.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserAnswerService } from './user-answer.service';
 import { UserAnswerController } from './user-answer.controller';
+import { UserAnswer } from './entities/user-answer.entity';
+import { User } from '../user/entities/user.entity';
+import { TreasureHunt } from '../treasure-hunt/entities/treasure-hunt.entity';
+import { TreasureHuntUser } from '../treasure-hunt/entities/treasure-hunt-user.entity';
+import { Location } from '../location/entities/location.entity';
+import { UserProgressModule } from '../user-progress/user-progress.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      UserAnswer,
+      User,
+      TreasureHunt,
+      TreasureHuntUser,
+      Location,
+    ]),
+    UserProgressModule,
+  ],
   controllers: [UserAnswerController],
   providers: [UserAnswerService],
 })
